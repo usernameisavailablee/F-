a8ad77efa83261945b58931ca395eddc86f33ac6
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-04-08 19:46:27 +0300

    сделал 13

--- LB_5/13/Program.fs
+++ LB_5/13/Program.fs
@@ -1,8 +1,40 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+    //поиск минимального
+let minf a =
+    let rec min a f=
+        if (a=0) then f
+        elif f > (a%10) then min (a/10) (a%10)
+        else min (a/10) f
+    min a a%10
+
+    //поиск максимального
+let maxf a =
+    let rec max a f=
+        if (a=0) then f
+        elif f < (a%10) then max (a/10) (a%10)
+        else max (a/10) f
+    max a 0
+
+    //хвостовая
+let comp a =
+    let rec f a c=
+        if a=0 then c
+        else  f (a/10) (c*(a%10))
+    f a 1
+
+    //вверх
+let rec comp1 a =
+    if a=0 then 1
+    else (a%10)*(comp1(a/10))
+    
+
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    (Console.ReadLine>>Convert.ToInt32>>minf>>Console.WriteLine)()
+    (Console.ReadLine>>Convert.ToInt32>>maxf>>Console.WriteLine)()
+    (Console.ReadLine>>Convert.ToInt32>>comp>>Console.WriteLine)()
+    (Console.ReadLine>>Convert.ToInt32>>comp1>>Console.WriteLine)()
+    0 
