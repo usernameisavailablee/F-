03d20c59fc100d35b8ec2438d19ee24157ebdb45
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-04-08 17:53:09 +0300

    12 сделал

--- LB_5/12/Program.fs
+++ LB_5/12/Program.fs
@@ -1,8 +1,22 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
-
+let answer s = 
+    System.Console.WriteLine()
+    match s with
+    |_ when s="1"||s="2" -> printfn "Подлиза" 
+    |_ when s="3"-> 
+        let s= System.Console.ReadLine()
+        if   s="Python" then printfn "питонисты..."
+        else printfn "Слушай, а неплохой выбор!"
+    
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    printfn "Какой у вас любимый язык программирования?"
+    printfn "1.Prolog"
+    printfn "2.F#"
+    printfn "3.Впишу свой"
+
+    (System.Console.ReadLine >> answer)()
+    
     0 // return an integer exit code
