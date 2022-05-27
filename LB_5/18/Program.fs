79fef2ffca199487fc83ff0546eeae2bbfcd6eeb
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-05-27 23:32:31 +0300

    18.2

--- LB_5/18/Program.fs
+++ LB_5/18/Program.fs
@@ -22,10 +22,18 @@ let maxdiv a  =
            max a newInit newdiv
     max a 0 a
 
+let comp a =
+    let rec c a init =
+        if a=0 then init else
+           let newInit = if (a%10)%5<>0 then init*(a%10) else init
+           let newCand=a/10
+           c newCand newInit 
+    c a 1
+
 [<EntryPoint>]
 let main argv =
     let a=System.Convert.ToInt32(System.Console.ReadLine())
     System.Console.WriteLine("Максимальный простой делитель числа:{0}",maxdiv a )
-
+    System.Console.WriteLine("Произведение цифр числа, не кратных 5:{0}",comp a)
     
     0 
