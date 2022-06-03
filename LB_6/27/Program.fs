9af149bc73705204c6e2057876101d540901ac0a
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-06-03 06:03:43 +0300

    27 complete

---
+++ LB_6/27/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = Convert.ToInt32(Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n= Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       Console.WriteLine(head)
+                       writeList tail  
+
+let rec Move (list: int list) = list.Tail@[list.Head]
+
+[<EntryPoint>]
+let main argv =
+    let list = readData
+    printfn "%A"(Move list)
+    0
