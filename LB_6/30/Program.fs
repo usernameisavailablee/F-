b8c846fe23351dba42bf50c7afe27775ddfc4158
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-06-03 07:49:19 +0300

    30 task

---
+++ LB_6/30/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = Convert.ToInt32(Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n= Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec loc_max list index =
+    match list with
+    | a::b::c::t when index = 1 -> b > a && b > c
+    | a::b::[] when index = 1 -> b > a
+    | a::b::t when index = 0 -> a > b
+    | _ -> 
+        let newIndex = index - 1
+        let newList = list.Tail
+        loc_max newList newIndex
+
+[<EntryPoint>]
+let main argv =
+    
+    let list = readData
+    Console.WriteLine("Индекс: ")
+    let index = Convert.ToInt32(Console.ReadLine())
+
+    printfn "%b"(loc_max list index)
+    0
